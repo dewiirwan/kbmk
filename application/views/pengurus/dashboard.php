d78a3f799bfb947193e3a1c6fe2c7ede5e15ecb3
Muhammad Alif Rhamadhan <50443591+malifrh@users.noreply.github.com>
2022-07-04 09:49:28 +0700

    Tab dashboard

--- application/views/pengurus/dashboard.php
+++ application/views/pengurus/dashboard.php
@@ -115,46 +115,6 @@
         <p><strong>&copy; <?php echo (date("Y")); ?> KBMK</strong><br /> Hak cipta dilindungi undang-undang.</p>
     </div>
 </div>
-
-<!-- Mainly scripts -->
-<script src="<?php echo base_url(); ?>assets/admin/js/jquery-3.1.1.min.js"></script>
-<script src="<?php echo base_url(); ?>assets/admin/js/bootstrap.min.js"></script>
-<script src="<?php echo base_url(); ?>assets/admin/js/plugins/metisMenu/jquery.metisMenu.js"></script>
-<script src="<?php echo base_url(); ?>assets/admin/js/plugins/slimscroll/jquery.slimscroll.min.js"></script>
-
-<!-- Flot -->
-<script src="<?php echo base_url(); ?>assets/admin/js/plugins/flot/jquery.flot.js"></script>
-<script src="<?php echo base_url(); ?>assets/admin/js/plugins/flot/jquery.flot.tooltip.min.js"></script>
-<script src="<?php echo base_url(); ?>assets/admin/js/plugins/flot/jquery.flot.spline.js"></script>
-<script src="<?php echo base_url(); ?>assets/admin/js/plugins/flot/jquery.flot.resize.js"></script>
-<script src="<?php echo base_url(); ?>assets/admin/js/plugins/flot/jquery.flot.pie.js"></script>
-<script src="<?php echo base_url(); ?>assets/admin/js/plugins/flot/jquery.flot.symbol.js"></script>
-<script src="<?php echo base_url(); ?>assets/admin/js/plugins/flot/jquery.flot.time.js"></script>
-
-<!-- Peity -->
-<script src="<?php echo base_url(); ?>assets/admin/js/plugins/peity/jquery.peity.min.js"></script>
-<script src="<?php echo base_url(); ?>assets/admin/js/demo/peity-demo.js"></script>
-
-<!-- Custom and plugin javascript -->
-<script src="<?php echo base_url(); ?>assets/admin/js/inspinia.js"></script>
-<script src="<?php echo base_url(); ?>assets/admin/js/plugins/pace/pace.min.js"></script>
-
-<!-- jQuery UI -->
-<script src="<?php echo base_url(); ?>assets/admin/js/plugins/jquery-ui/jquery-ui.min.js"></script>
-
-<!-- Jvectormap -->
-<script src="<?php echo base_url(); ?>assets/admin/js/plugins/jvectormap/jquery-jvectormap-2.0.2.min.js"></script>
-<script src="<?php echo base_url(); ?>assets/admin/js/plugins/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
-
-<!-- EayPIE -->
-<script src="<?php echo base_url(); ?>assets/admin/js/plugins/easypiechart/jquery.easypiechart.js"></script>
-
-<!-- Sparkline -->
-<script src="<?php echo base_url(); ?>assets/admin/js/plugins/sparkline/jquery.sparkline.min.js"></script>
-
-<!-- Sparkline demo data  -->
-<script src="<?php echo base_url(); ?>assets/admin/js/demo/sparkline-demo.js"></script>
-
 <!-- Page-Level Scripts -->
 <!-- <script>
     $(document).ready(function() {
